ca40a3d00f19b807d1a923118fd25ad1f97aa2c0
Toufiqul <fahim.prime@gmail.com>
2024-01-05 01:52:33 +0600

    fixed by erasing  const [count, setCount] = useState(0) in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Success from './components/Success'
 import Cancel from './components/Cancel'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
 <BrowserRouter>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-        Home
-    </div>
+<div className='w-full mx-auto'>
+      <div className='text-center font-raleway w-full max-w-5xl mx-auto my-6'>
+        <div className='font-extrabold text-transparent text-6xl my-10 bg-clip-text
+        bg-gradient-to-r from-yellow-400 to-yellow-800'>
+            Chocolate Corner
+        </div>
+        </div>
+        </div>
   )
 }
 
