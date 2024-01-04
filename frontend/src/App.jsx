0311370ff76db9dbfb0912ceb0923a2d68e68a03
Toufiqul <fahim.prime@gmail.com>
2024-01-05 03:15:30 +0600

    fix: no display
    import useState from react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Success from './components/Success'
 import Cancel from './components/Cancel'
+import { useState } from 'react'
 
 function App() {
-
+    const [count, setCount] = useState(0)
   return (
 <BrowserRouter>
 <Routes>
